7ecf6b502f78a495557f1ad6088fac1506533677
김현석 <sidosea470@gmail.com>
2025-05-05 18:10:44 +0900

    \ -> ₩

--- front/order_done.js
+++ front/order_done.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
   const orderNumber = 1; //샘플넘버
   $("#order-number").text(orderNumber);
+// 결제 방식 클릭 이벤트
+  $("#order-done").on("click", function () {
+     window.location.href = "Frontpage.html";
+  });
 
   // 5초 후 자동으로 메인 페이지로 이동
   setTimeout(function () {
