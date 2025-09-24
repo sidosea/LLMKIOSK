f9660a4e9e2131b1d99cc07c8fb853a9d14a3f6d
김현석 맥북 <sidosea470@gmail.com>
2025-09-24 23:16:51 +0900

    https 로드밸런서 변경

--- front/main.js
+++ front/main.js
@@ -4,7 +4,7 @@ const cart = {};
 
 //배포 후 연결 || 로컬에서 테스트
 // const API_BASE_URL = 'http://localhost:5000';
-const API_BASE_URL = 'http://kiosk-server-env.eba-as7cmwjg.ap-northeast-2.elasticbeanstalk.com';
+const API_BASE_URL = 'https://kiosk-server-env.eba-as7cmwjg.ap-northeast-2.elasticbeanstalk.com';
 
 // 가격 포맷
 function formatPrice(price) {
