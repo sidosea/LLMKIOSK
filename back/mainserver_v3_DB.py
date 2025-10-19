a1a9bc51e8a344e0ed7fa2cdc76bc45037040eea
김현석 맥북 <sidosea470@gmail.com>
2025-10-19 16:27:26 +0900

    GET POST 오류 해결

--- back/mainserver_v3_DB.py
+++ back/mainserver_v3_DB.py
@@ -246,4 +246,4 @@ def post_recommendations():
 app.register_blueprint(api_v1)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5001, debug=True)
---
+++ back/run_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /Users/hyunsuk/Desktop/documents/LLMKIOSK/back
+source venv/bin/activate
+python mainserver_v3_DB.py
+
+
+
